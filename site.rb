7b9609d5d2483d4e75a9485623f6b230a37df41b
Erica <garciadesign.me@gmail.com>
2014-11-17 15:45:34 -0600

    Added images for pages, pages for results, and worked on making website respnsive

--- site.rb
+++ site.rb
@@ -14,15 +14,28 @@ get '/flights' do
   haml :flights
 end
 
+get '/flight_results' do
+    haml :flight_results
+end    
+
 get '/hotels' do
   haml :hotels
 end
+
+get '/hotel_results' do
+  haml :hotel_results
+end  
 
 get '/cars' do
   haml :cars
 end
 
+get '/car_results' do
+  haml :car_results
+end  
+
 get '/deals' do
   haml :deals
 end
+
 
