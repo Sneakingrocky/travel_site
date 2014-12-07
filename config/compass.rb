60e8b667aa7231157aebc2db3788f77461586336
Erica <garcialuck08@gmail.com>
2014-12-07 13:07:29 -0600

    Remove foundation and create my own grid system

--- config/compass.rb
+++ config/compass.rb
@@ -1,7 +1,6 @@
 css_dir = File.join 'public', 'css'
 relative_assets = true
 
-# This is common configuration
 sass_dir = File.join 'views', 'stylesheets'
 images_dir = File.join 'public', 'images'
 http_path = "/"
