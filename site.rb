17975e9aade4c10eafa3bdb1543fdcfd5e510d7c
Erica <garciadesign.me@gmail.com>
2014-12-11 00:47:01 -0600

    Added active class to navigation items to show current page

--- site.rb
+++ site.rb
@@ -11,30 +11,37 @@ get '/' do
 end
 
 get '/flights' do
+  @flights_tab = true
   haml :flights
 end
 
 get '/flight_results' do
+  @flights_tab = true
     haml :flight_results
 end    
 
 get '/hotels' do
+  @hotels_tab = true
   haml :hotels
 end
 
 get '/hotel_results' do
+  @hotels_tab = true
   haml :hotel_results
 end  
 
 get '/cars' do
+  @cars_tab = true
   haml :cars
 end
 
 get '/car_results' do
+  @cars_tab = true
   haml :car_results
 end  
 
 get '/deals' do
+  @deals_tab = true
   haml :deals
 end
 
